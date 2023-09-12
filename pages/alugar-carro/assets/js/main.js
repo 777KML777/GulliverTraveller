382f2533e4f0272a9257dfaf66d5b11874b7c028
Leonardo de Souza <leonardodesouzaalmeidas@gmail.com>
2023-09-12 20:14:33 -0300

    Funcoes Com javascript adicionadas, pasta Before Code adicionada

--- pages/alugar-carro/assets/js/main.js
+++ pages/alugar-carro/assets/js/main.js
@@ -9,3 +9,61 @@ function sideShow() {
 function sideHide() {
     filtroBarra.style.display = 'none'
 }
+
+
+localForm = document.querySelectorAll('[data-input-local]')
+dataForm = document.querySelectorAll('[data-input-data]')
+
+let tamanhoJanela = window.innerWidth
+window.addEventListener("resize", () => {
+    tamanhoJanela = window.innerWidth
+    if (tamanhoJanela > 1440 || tamanhoJanela < 1024) {
+        for (let i = 0; i < dataForm.length; i++) {
+            dataForm[i].style.display = "flex";
+            dataForm[i].parentNode.style.width = "100%"
+            dataForm[i].parentNode.parentNode.style.width = "100%"
+            localForm[i].style.display = "flex"
+            localForm[i].parentNode.style.width = "100%"
+            localForm[i].parentNode.parentNode.style.width = "auto"
+            localForm[i].parentNode.parentNode.style.width = "100%"
+        }
+    } else {
+        for (let i = 0; i < dataForm.length; i++) {
+            if (dataForm[i].style.display === "flex" & localForm[i].style.display === "flex") {
+                dataForm[i].style.display = "none";
+                dataForm[i].parentNode.style.width = "fit-content"
+                dataForm[i].parentNode.parentNode.style.width = "fit-content"
+            }
+        }
+    }
+})
+
+function toggleData() {
+    if (tamanhoJanela < 1440 & tamanhoJanela > 1024) {
+        for (let i = 0; i < dataForm.length; i++) {
+            if (dataForm[i].style.display === "none") {
+                dataForm[i].style.display = "flex";
+                localForm[i].style.display = "none"
+                dataForm[i].parentNode.style.width = "100%"
+                dataForm[i].parentNode.parentNode.style.width = "100%"
+                localForm[i].parentNode.style.width = "fit-content"
+                localForm[i].parentNode.parentNode.style.width = "auto"
+            } else {
+                dataForm[i].style.display = "none";
+                dataForm[i].parentNode.style.width = "fit-content"
+                dataForm[i].parentNode.parentNode.style.width = "fit-content"
+                localForm[i].style.display = "flex"
+                localForm[i].parentNode.style.width = "100%"
+                localForm[i].parentNode.parentNode.style.width = "inherit"
+            }
+        }
+    } else {
+        for (let i = 0; i < dataForm.length; i++) {
+            dataForm[i].style.display = "flex";
+            localForm[i].style.display = "flex"
+            localForm[i].parentNode.style.width = "100%"
+            localForm[i].parentNode.parentNode.style.width = "100%"
+            dataForm[i].parentNode.parentNode.style.width = "100%"
+        }
+    }
+}
